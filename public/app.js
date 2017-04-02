1d23c9a1acba65f0ebbfbd9e8a3a6e170ac72893
Dave Harned <dharned@gmail.com>
2017-04-02 12:52:51 -0500

    add body parser and receive post request

--- public/app.js
+++ public/app.js
@@ -3,10 +3,11 @@
         .module("BlogApp", [])
         .controller("BlogController", BlogController);
 
-    function BlogController($scope) {
+    function BlogController($scope, $http) {
         $scope.createPost = function (post) {
             console.log("post.title: ", post.title);
-            console.log("post.body: ", post.body);            
+            console.log("post.body: ", post.body);   
+            $http.post("/api/blogpost", post);
         }
     }
 })();
