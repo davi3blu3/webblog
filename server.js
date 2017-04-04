ae3206684cabf5aed0952c74c04620cbe8d4cdad
Dave Harned <dharned@gmail.com>
2017-04-03 22:41:27 -0500

    connecting to mongo

--- server.js
+++ server.js
@@ -2,11 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/webblog')
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// connect mongodb
+mongoose.connect('mongodb://localhost/mywebblog', function(error){
+    if (error) {
+        process.on('exit', (code) => {
+            console.log(`About to exit with code: ${code}`);
+        });
+    }
+});
 
 app.post('/api/blogpost', function(req, res) {
     console.log('hello from server - post received');
